b2abe7223b7c3893fc074af4c6cf391c94912eca
DeanYoon <68269605+DeanYoon@users.noreply.github.com>
2022-09-22 10:04:47 +0900

    9/21

--- src/server.js
+++ src/server.js
@@ -1,10 +1,23 @@
 import express from "express";
-
+import morgan from "morgan";
 const PORT = 4000;
 
-const app = express();
+const app = express(); //Create Application
+const logger = morgan("dev");
+
+function handleHome(req, res) {
+  return res.send("Hello!");
+}
+
+function login(req, res) {
+  return res.send("login");
+}
+
+app.use(logger);
+app.get("/", handleHome);
+app.get("/login", login);
 
 const handleListening = () =>
   console.log(`Server listening on port http://localhost:${PORT} !!`);
 
-app.listen(PORT, handleListening);
+app.listen(PORT, handleListening); //외부 접속 리슨
