3274513a85e0f58eef0b844f65e5b7dfabf54c07
DeanYoon <68269605+DeanYoon@users.noreply.github.com>
2022-09-30 16:01:28 +0900

    '7.0-7.3'

--- src/db.js
+++ src/db.js
@@ -1,6 +1,8 @@
 import mongoose from "mongoose";
 
-mongoose.connect("mongodb://127.0.0.1:27017/wetube", {});
+mongoose.connect("mongodb://127.0.0.1:27017/wetube", {
+  useNewUrlParser: true,
+});
 
 const db = mongoose.connection;
 
